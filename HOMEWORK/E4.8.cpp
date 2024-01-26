2fd40a486daaa0a4a662158e8ab12ec6da96dca2
Andy <andycocha6@gmail.com>
2024-01-26 10:17:35 -0500

    COMPLETED HW BUT NOT ASSIGNED ON GRADESCOPE YET

---
+++ HOMEWORK/E4.8.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Andy Cocha
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: E4.8
+DATE: 1/26/2024
+
+Hw: reads a word and prints each character on a separate line
+*/
+
+#include <iostream>
+#include <string>
+
+
+int main(){
+    std::string word;
+
+    //ask user for input
+    std::cout << "Enter word: ";
+    std::cin >> word;
+
+    
+    //print each character from word on a separate line
+    for (int i = 0; i < word.length(); i++){ //iterate & print each character
+        std::cout << word[i] << std::endl;
+    }
+
+    return 0;
+}
