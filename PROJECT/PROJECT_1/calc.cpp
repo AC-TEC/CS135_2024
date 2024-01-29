f631ed184d0fce08435f4c56ca1a80c14277110a
Andy <andycocha6@gmail.com>
2024-01-29 10:43:03 -0500

    COMPLETED TASK B LAB 1

--- PROJECT/PROJECT_1/calc.cpp
+++ PROJECT/PROJECT_1/calc.cpp
@@ -5,28 +5,36 @@ Instructor: Tong Yi
 Assignment: Project 1 
 DATE: 1/26/2024
 
-Part B: 
+Part B: read non-negative integers written to be addded or subtacted and print out sum
 */
 
 #include <iostream>
 
-
-
 int main(){
-    char something; //for storing the symbols
-    int num;// for storing number
-    int track = 0; //where we total everything
+    char read; //read the txt / read the - or + symbols
+    int sum = 0; //keep track of sum
+    
+    //get first number in txt file
+    int num;
+    std::cin >> num;
+    sum += num;
 
+    //iterate through the txt file
+    while(std::cin >> read){
+        //get the integers
+        int number;
+        std::cin >> number;
 
-    while(std::cin >> num || std::cin >> something){
-        if (something == '+'){
-            track += num;
-            std::cout << "plus: " << track << std::endl;
+    //Do operation based on + or - characters
+        if (read == '+'){
+            sum += number;
         } else{
-            track -= num;
-            std::cout << "subtract: " << track << std::endl;
+            sum -= number;
         }
-    }
+    } 
 
-    std::cout << "Final: " << track << std::endl;
+    //print out sum
+    std::cout << "SUM: " << sum << std::endl;
+    
+    return 0;
 }
