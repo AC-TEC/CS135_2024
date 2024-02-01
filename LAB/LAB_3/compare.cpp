26fd6a169aad156809fa95faf1740ead6ac639d4
Andy <andycocha6@gmail.com>
2024-01-31 22:43:49 -0500

    METHOD 2 SUBMITTED ON GRADESCOPE

--- LAB/LAB_3/compare.cpp
+++ LAB/LAB_3/compare.cpp
@@ -13,7 +13,6 @@ Task C: ask user for two dates(beginning and end) then check each day in the int
 #include <string>
 #include <fstream> //read file
 #include <cstdlib> //close() file
-#include <sstream> // to parse each data(column) from each line (row)
 
 int main(){
 
@@ -33,7 +32,7 @@ int main(){
     //ask for user starting and end dates (format: MM/DD/YYYY)
     std::string starting_date;
     std::string ending_date;
-    
+
     std::cout << "Enter starting date: ";
     std::cin >> starting_date;
     std::cout << "Enter ending date: ";
@@ -41,59 +40,11 @@ int main(){
 
 
     //variables to store tsv data
-    std::string line;
     std::string date;
     double eastEl;
     double westEl;
 
-
-    //parse through tsv file 
-    while(getline(water_data,line)){
-        //create a input stream to go through each column data
-        std::istringstream column(line);
-
-        //get first column data (date)
-        column >> date;
-        //skip other column
-        column >> junk_line;
-        //get east elevation data
-        column >> eastEl;
-        //skip other column
-        column >> junk_line;
-        //get west elevation data
-        column >> westEl;
-    
-        //
-        if (date >= starting_date && date <= ending_date) {
-            //compare the elevation levels and print out the higher one
-            if(eastEl < westEl){
-                std::cout << date << " " << "West" << std::endl;
-            } else if (westEl < eastEl){
-                std::cout << date << " " << "East" << std::endl;
-            } else {
-                std::cout << date << " " << "Equal" << std::endl;
-            }
-        }
-    }
-
-    //close file
-    water_data.close();
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-/*
-
-?other easier method (wont need to include sstream library)
+    //read each line(row) individually from file
     while(water_data >> date >> junk_line >> eastEl >> junk_line >> westEl){
         //get date interval range
         if (date >= starting_date && date <= ending_date){
@@ -106,4 +57,10 @@ int main(){
             }
         }
     }
-*/
+
+    //close file
+    water_data.close();
+
+    return 0;
+}
+
