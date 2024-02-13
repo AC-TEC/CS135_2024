71f923f5563d5abc55c5e7b3cfa260b9b1b7edef
Andy <andycocha6@gmail.com>
2024-02-13 16:47:06 -0500

    COMPLETED TASK B AND C

--- LAB/LAB_5/numbers.cpp
+++ LAB/LAB_5/numbers.cpp
@@ -3,14 +3,13 @@ Author: Andy Cocha
 Course: CSCI-135
 Instructor: Tong Yi
 Assignment: LAB 5
-DATE: 2/12/2024
+DATE: 2/13/2024
 
-Task A: write a function that takes in two integers and checks if they are divisible by each other.
-        If they are then print yes else print no.
+Task C: make a function that returns the next prime number.
 */
 #include <iostream>
 
-
+//TASK A
 bool isDivisibleBy(int n, int d){
     //check for division by 0
     if(d == 0){
@@ -25,6 +24,59 @@ bool isDivisibleBy(int n, int d){
 
     return false;
 }
+
+
+
+//TASK B
+bool isPrime(int n){
+
+    //check if number is less than 2
+    if(n < 2){
+        return false;
+    }
+
+    //check if number is able to be divided evenly by other than 2 and itself. if it is able to be divided then not a prime number
+    for(int i = 2; i < n; i++){
+        if(isDivisibleBy(n,i) == true){
+            return false;
+        }
+    }
+
+    //number can only be divided by 1 and itself. NO other numbers
+    return true; //prime number
+}
+
+
+
+
+//TASK C
+int nextPrime(int n){
+    //starting point (bc next prime number has to be greater than the one being passed into parameter)
+    int next_number = n+1;
+
+    //if next_number is false then keep on going until we get a prime number
+    while(isPrime(next_number) == false){
+        next_number++;
+    }
+
+    //returns the next prime number
+    return next_number;
+}
+
+
+
+int main(){
+
+
+}
+
+
+
+
+
+/*
+
+TASK A MAIN FUNCTION BELOW:
 
 int main(){
     int n;
@@ -48,3 +100,55 @@ int main(){
 
     return 0;
 }
+
+*/
+
+
+
+
+/*
+TASK B MAIN FUNCTION BELOW:
+
+    int number;
+
+    //get user input
+    std::cout << "Enter number: ";
+    std::cin >> number;
+
+    //call function
+    bool is_prime = isPrime(number);
+
+    //print if number is prime
+    if(is_prime == 1){    //(1) == true
+        std::cout << "true" << std::endl;
+    }else{                //(0) == false
+        std::cout << "false" << std::endl;
+    }
+
+    return 0;
+
+*/
+
+
+/*
+TASK C MAIN FUNCTION BELOW:
+
+int main(){
+
+    int number;
+
+    //get user input
+    std::cout << "Enter number: ";
+    std::cin >> number;
+
+    //call function
+    int new_number = nextPrime(number);
+
+    //print out number after calling function
+    std::cout << new_number << std::endl;
+
+    return 0;
+}
+
+
+*/
