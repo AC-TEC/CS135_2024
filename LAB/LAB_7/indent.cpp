dd093e588843ec1d38bfd2f7bc1359ff87001aed
Andy <andycocha6@gmail.com>
2024-02-26 10:58:21 -0500

    countChar function is working properly

--- LAB/LAB_7/indent.cpp
+++ LAB/LAB_7/indent.cpp
@@ -11,11 +11,30 @@ Task B:
 #include <iostream>
 #include <string>
 
+//function to count how many times a character appears
 int countChar(std::string line, char c){
+    //need counter to track number of occurrences
+    int counter = 0;
 
+    //iterate through the line
+    for(int i = 0; i < line.length(); i++){
+        //check if character at current index matches the character given
+        if(line[i] == c){
+            //add to counter
+            counter++;
+        }
+    }
+
+    //return the amount of occurences
+    return counter;
 }
 
 
 int main(){
-    
+    std::string line;
+
+    //go through each line
+    while(getline(std::cin,line)){
+        std::cout << countChar(line,'{') << " " << line << std::endl;
+    }
 }
