ee4eb0e11ef83e03751a448277a05554039b50d2
Andy <andycocha6@gmail.com>
2024-02-26 10:14:34 -0500

    FINISHED

---
+++ LAB/LAB_7/bad-code.cpp
@@ -0,0 +1,13 @@
+//for use of TASK A LAB 7
+//02/26/2024
+
+#include <iostream>
+using namespace std;
+             int main(){
+           // Hi, I'm a program!
+int x = 1; 
+    for(int i = 0; i < 10; i++) {
+cout << i;
+          cout << endl;
+ }
+    }
