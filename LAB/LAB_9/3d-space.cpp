354a9cdc50392d01dc96b6231325ffdf34801d17
Andy <andycocha6@gmail.com>
2024-03-14 14:03:40 -0400

    COMPLETED LAB 9C

--- LAB/LAB_9/3d-space.cpp
+++ LAB/LAB_9/3d-space.cpp
@@ -46,23 +46,24 @@ Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
 
 
 //*TASK C
+//p->x would access the value of the x member of the object pointed to by p
 void move(Coord3D *ppos, Coord3D *pvel, double dt){
-
+    ppos->x = ppos->x + pvel->x * dt;
+    ppos->y = ppos->y + pvel->y * dt;
+    ppos->z = ppos->z + pvel->z * dt;
 }
 
 int main() {
-    Coord3D pointP = {10, 20, 30};
-    Coord3D pointQ = {-20, 21, -22};
 
-    std::cout << "Address of P: " << &pointP << std::endl;
-    std::cout << "Address of Q: " << &pointQ << std::endl << std::endl;
+    Coord3D pos = {0, 0, 100.0};
+    Coord3D vel = {1, -5, 0.2};
 
-    Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+    move(&pos, &vel, 2.0); // object pos gets changed
+    std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+    // prints: 2 -10 100.4
 
-    //prints out the address
-    std::cout << "ans = " << ans << std::endl; // So which point is farther?
+    return 0;
 }
-
 
 
 /*
