45146b1152daf295920bb6f98782d684c1aaefdc
Andy <andycocha6@gmail.com>
2024-04-01 22:31:12 -0400

    HPP FILE ADDED

--- PROJECT/PROJECT_2/MemoryGame.hpp
+++ PROJECT/PROJECT_2/MemoryGame.hpp
@@ -10,44 +10,55 @@ public: //public method member, any class can use these methods
        //default constructor, with 3 pairs of random integers in range [0, 999] 
        //placed in 8 blocks (two blocks are empty).
     
+
     MemoryGame(int numPairs, int numSlots);
        //Place numPairs pairs of random integers in range [0, 999] in numSlots space, need numPairs > 0, numSlots > 0, and numSlots >= 2 * numPairs
     
+
     MemoryGame(string *words, int size, int numSlots);
        //instead of randomly generated integers,
        //use words as data
     
+
     ~MemoryGame();
     
+
     void play();
     
+
     void display() const;
          //display array values, if bShown[i] is true,
          //then values[i] is displayed, where i is the index.
     
+
     void randomize();
         //randomize the layout of elements in values.
     
+
     int input() const;
         //input an int that is a valid index and 
         //the corresponding element of values is not shown yet.
         //That is, the input i is in [0, numSlots) and
         //bShown[i] is false. 
 
+
 private: //private data members, private means that
          //only methods in this class, not other class,
          //can access or modify these data members.
     int numPairs; //numPairs of identical twin items
-    
+
+
     int numSlots;
         //size of array value, besides identical twins,
         //may contain empty string to
         //make the problem more challenging
-    
+
+
     string *values;
         //a string to represent the layout of data,
         //mixed with possible empty strings.
         //Use array to access each element in const time.
+
     
     bool *bShown;
         //an array of boolean to indicate which element of
