e2f88b07af939f030e41d58f94a31a0145f58e19
Andy Cocha <andycocha@Rouge-MacBook.lan>
2024-05-13 23:32:55 -0400

    COMPLETED TASK B

--- LAB/LAB_13/recursion.cpp
+++ LAB/LAB_13/recursion.cpp
@@ -3,12 +3,16 @@ Author: Andy Cocha
 Course: CSCI-135
 Instructor: Tong Yi
 Assignment: LAB 13
-DATE: 5/09/2024
+DATE: 5/13/2024
 
 Task A: Write a program that prints all numbers in range left to right, separated by spaces. (Don’t use loops, global or static variables.)
+
+TASK B: Write a program to add the numbers from a given range. From left to right
+
 */
 #include <iostream>
 
+//?TASK A
 //function to recursivly call range of given numbers
 void printRange(int left, int right){
     
@@ -27,8 +31,57 @@ void printRange(int left, int right){
     }
 }
 
+//?TASK B
+//function to recursivly add the numbers from a given range (left to right)
+int sumRange(int left, int right){
+    if (left > right){ //Base case
+        return 0;
+    } else{
+        return left + sumRange(left + 1, right); //Recursion(Keeps on going until the base case is true)
+    }
+}
+
+
+
+int main(){
+    int left;
+    int right;
+
+//Test #1
+    int x = sumRange(1, 3);
+    std::cout << "This is " << x << std::endl;
+
+//Test #2
+    int y = sumRange(-2, 10);
+    std::cout << "This is " << y << std::endl;
+}
+
+
+/*
+!TASK A MAIN FUNCTION
 
 int main(){
     //call recursive function
     printRange(-2, 10);
 }
+
+*/
+
+
+/*
+!TASK B MAIN FUNCTION
+
+int main(){
+    int left;
+    int right;
+
+//Test #1
+    int x = sumRange(1, 3);
+    std::cout << "This is " << x << std::endl;
+
+//Test #2
+    int y = sumRange(-2, 10);
+    std::cout << "This is " << y << std::endl;
+}
+
+*/
