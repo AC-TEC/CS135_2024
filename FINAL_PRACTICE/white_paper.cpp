6a89f2a3bcab71f0176e6215cf33eba53b02f50e
Andy Cocha <andycocha@Rouge-MacBook.lan>
2024-05-21 21:16:51 -0400

    FINAL

---
+++ FINAL_PRACTICE/white_paper.cpp
@@ -0,0 +1,24 @@
+/*
+?Question 1: 
+Suppose class Dog is derived from class Animal, which class is a superclass
+
+!Answer: 
+Animal is the superclass
+*/
+
+
+
+/*
+?Question 2:
+Declare an array pf strings, call is Shapes. Initialize with "Square","Triangle"
+
+!Answer:
+Std::string Shapes[] = {"Square", "Triangle"};
+*/
+
+
+/*
+?Question 3:
+Write code to print 1,4,16,...,4^15, where the next item is four times of the previous one.
+*/
+
